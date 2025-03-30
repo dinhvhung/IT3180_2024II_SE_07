d9579609d3daeab2a0ff2ecd8a6fdc951869e314
DoVanTai123 <161468185+DoVanTai123@users.noreply.github.com>
2025-03-30 22:57:08 +0700

    Resident.java

--- demo.xsx/demo.xsx/src/main/java/com/example/service_apa/demo/xsx/Entity/Resident.java
+++ demo.xsx/demo.xsx/src/main/java/com/example/service_apa/demo/xsx/Entity/Resident.java
@@ -3,19 +3,24 @@ package com.example.service_apa.demo.xsx.Entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import java.util.List;
 
 @Entity
 @Getter
 @Setter
+@Table(name = "residents")
 public class Resident {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
+    private String fullName;
+
+    private String email;
+
+    private String phone;
+
     private String apartmentNumber;
 
-    @OneToMany(mappedBy = "resident")
-    private List<Invoice> invoices;
+   
 }
