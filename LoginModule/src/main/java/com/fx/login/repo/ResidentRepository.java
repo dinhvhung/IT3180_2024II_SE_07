a9ae2b0742d9b26c49c1cd809d110cb034ebf793
Dinh Viet Hung <dvhung090705@gmail.com>
2025-05-20 19:35:08 +0700

    App

--- LoginModule/src/main/java/com/fx/login/repo/ResidentRepository.java
+++
@@ -1,5 +0,0 @@
-package com.fx.login.repo;
-import com.fx.login.model.ResidentEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ResidentRepository extends JpaRepository<ResidentEntity, Long> {}
--- resident/src/main/java/com/example/resident/ResidentRepository.java
+++
@@ -1,4 +0,0 @@
-package com.example.resident;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ResidentRepository extends JpaRepository<ResidentEntity, Long> {}
